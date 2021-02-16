4800e93334708cd26436d43f3c55507bc39a060d
yinruei <yinruei@g.ncu.edu.tw>
2021-02-17 03:46:22 +0800

    fix 括號

--- homework/day_02.py
+++ homework/day_02.py
@@ -4,8 +4,8 @@ import numpy as np
 array1 = np.array(range(30))
 
 #1. 將上列陣列(array1)，轉成維度為(5X6)的 array，順序按列填充。(hint:order="F")
-print(array1.reshape((5, 6), order='F'))
+print(array1.reshape(5, 6, order='F'))
 
 #2. 呈上題的 array，找出被 6 除餘 1 的數的索引
-a = array1.reshape((5, 6), order='F')
+a = array1.reshape(5, 6, order='F')
 print( np.where(a %6== 1))
