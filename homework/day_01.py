8a8b6684c45c76ef7de0e01207b0f5dd4cba2967
yinruei <yinruei@g.ncu.edu.tw>
2021-02-17 03:49:46 +0800

    修改寫法

--- homework/day_01.py
+++ homework/day_01.py
@@ -1,12 +1,11 @@
 import numpy as np
 
 # 1. 生成一個等差數列，首數為 0，尾數為 20，公差為 1 的數列。
-print(np.arange(0, 21, 1))
-
+a = np.arange(0, 21)
+print(a)
 # 2. 呈上題，將以上數列取出偶數。
-print(np.arange(0, 21, 2))
+print(a[::2])
 
 # 3. 呈 1 題，將數列取出 3 的倍數。
-a = np.arange(0, 21, 1)
 
-print(a[::3])
+print(a[3::3])
